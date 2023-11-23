8bbbd1bbce3ae197bf21ec6b71256251ae019997
Genuss-design23 <151791642+Genuss-design23@users.noreply.github.com>
2023-11-23 12:03:20 +0100

    JavaScript file aded
    Dodat JavaScript fajl za pocetnu stanicu.

---
+++ pocetna.js
@@ -0,0 +1,23 @@
+var countDownDate = new Date("Mar 4, 2024 10:00:00").getTime();
+
+var x = setInterval(function() {
+
+  var now = new Date().getTime();
+
+  var distance = countDownDate - now;
+
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the result in the element with id="demo"
+  document.getElementById("demo").innerHTML = days + "д " + hours + "с "
+  + minutes + "мин " + seconds + "с ";
+
+  // If the count down is finished, write some text
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = "EXPIRED";
+  }
+}, 1000);
